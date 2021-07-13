8a44dc7b8caeb5ed43f789b88050641f635f1d7a
loc nguyen <loc.nguyenqa28@gmail.com>
2021-07-13 09:19:43 +0700

    implement 3 lesions

--- cypress/integration/parallel-spec.js
+++ cypress/integration/parallel-spec.js
@@ -98,6 +98,55 @@ describe("parallel", () => {
     homeActions.isDashboardDisplayed();
     // dashboardActions.isUploadSuccesfully(0);
   });
+ 
+  it("3.Large images 4 lesions + 1 images", () => 
+  {
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+    
+    //Add New Lesion - Patient Details
+    const firstname = `parallel ${homeActions.randomAlpha(20)}`;
+    const lastname = `three lesions-one large images`;
+    dashboardActions.clickAddNewLesion();
+    dashboardActions.selectTitle('Mrs');
+    dashboardActions.enterFirstName(firstname);
+    dashboardActions.enterLastName(lastname);
+    dashboardActions.selectGender(user.gender.unknown);
+    dashboardActions.enterDOB(user.DOB);
+    dashboardActions.enterHomeAdd(user.address);
+    dashboardActions.enterCity(user.city);
+    dashboardActions.selectState();
+    dashboardActions.enterPostcode(user.postcode);
+    dashboardActions.enterContact(user.contact);
+    dashboardActions.enterMedicare(user.medicare);
+    dashboardActions.nextButton();
+
+    //Add lesion
+    dashboardActions.addALesionLargeImages(1);
+
+    //Add another lesion
+    dashboardActions.addAnotherLesion()
+    dashboardActions.addALesionLargeImages(1)
+
+    //Add another lesion
+    dashboardActions.addAnotherLesion()
+    dashboardActions.addALesionLargeImages(1)
+
+    //Add another lesion
+    dashboardActions.addAnotherLesion()
+    dashboardActions.addALesionLargeImages(1)
+
+    //Case Summary
+    dashboardActions.caseSummary();
+    dashboardActions.selectGender(user.gender.other);
+    dashboardActions.submitCasePrint();
+    dashboardActions.returnToDashboard();
+    homeActions.isDashboardDisplayed();
+    // dashboardActions.isUploadSuccesfully(0);
+  });
 
 });
   
