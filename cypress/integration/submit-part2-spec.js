e76037ea2d699c4a51efe3a6ac607ff2a6d056a4
loc nguyen <loc.nguyenqa28@gmail.com>
2021-09-30 08:40:51 +0700

    Updated submit

--- cypress/integration/submit-part2-spec.js
+++ cypress/integration/submit-part2-spec.js
@@ -1282,49 +1282,7 @@ describe("Add New Lesion on eDerm", () => {
     dashboardActions.isUploadSuccesfully(0);
   });
 
-  it("53. Private to Schedule Fee", () => 
-  {
-    loginActions.visitPage();
-    loginActions.inputUserName(user.username);
-    loginActions.inputPassword(user.password);
-    loginActions.clickLoginButton();
-    homeActions.isDashBoardButtonDisplayed();
-    
-    //Add New Lesion - Patient Details
-    const firstname = `submit ${homeActions.randomAlpha(10)}`;
-    const lastname = `private to Schedule Fee`;
-    dashboardActions.selectClinicOptionByName();
-    dashboardActions.clickOkSelectClinic();
-    dashboardActions.clickAddNewLesion();
-    dashboardActions.selectTitle('Mrs');
-    dashboardActions.enterFirstName(firstname);
-    dashboardActions.enterLastName(lastname);
-    dashboardActions.selectGender('Unknown');
-    dashboardActions.enterDOB(user.DOB);
-    dashboardActions.enterHomeAdd(user.address);
-    dashboardActions.enterCity(user.city);
-    dashboardActions.selectState();
-    dashboardActions.enterPostcode(user.postcode);
-    dashboardActions.enterContact(user.contact);
-    dashboardActions.selectBilling(user.billing.private)
-    dashboardActions.checkPrivate();
-   
-    dashboardActions.nextButton();
-
-    //Add first lesion
-    dashboardActions.addALesionByNumberImages(1);
-
-    // Case Summary
-    dashboardActions.caseSummary();
-    dashboardActions.editToScheduleFee();
-   
-    dashboardActions.submitCasePrint();
-    dashboardActions.returnToDashboard();
-    homeActions.isDashboardDisplayed();
-    dashboardActions.isUploadSuccesfully(0);
-  });
-
-  it("54. Schedule Fee to Bulk bill", () => 
+  it("53. Schedule Fee to Bulk bill", () => 
   {
     loginActions.visitPage();
     loginActions.inputUserName(user.username);
@@ -1367,9 +1325,97 @@ describe("Add New Lesion on eDerm", () => {
     dashboardActions.returnToDashboard();
     homeActions.isDashboardDisplayed();
     dashboardActions.isUploadSuccesfully(0);
+  });  
+    
+  it("54. Schedule Fee to DVA", () => 
+  {
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+    
+    //Add New Lesion - Patient Details
+    const firstname = `submit ${homeActions.randomAlpha(10)}`;
+    const lastname = `Schedule Fee to DVA`;
+    dashboardActions.selectClinicOptionByName();
+    dashboardActions.clickOkSelectClinic();
+    dashboardActions.clickAddNewLesion();
+    dashboardActions.selectTitle('Mrs');
+    dashboardActions.enterFirstName(firstname);
+    dashboardActions.enterLastName(lastname);
+    dashboardActions.selectGender('Unknown');
+    dashboardActions.enterDOB(user.DOB);
+    dashboardActions.enterHomeAdd(user.address);
+    dashboardActions.enterCity(user.city);
+    dashboardActions.selectState();
+    dashboardActions.enterPostcode(user.postcode);
+    dashboardActions.enterContact(user.contact);
+    dashboardActions.fillScheduleFeeInPatientDetails();
+   
+    dashboardActions.nextButton();
+
+    //Add first lesion
+    dashboardActions.addALesionByNumberImages(1);
+
+    //Case Summary
+    dashboardActions.caseSummary();
+
+    dashboardActions.clickEditPatientDetails();
+    dashboardActions.selectBilling(user.billing.DVA)
+    dashboardActions.enterDVANumber(validDVANumber);
+    dashboardActions.clickSavePatientDetails();
+    dashboardActions.clickOkPatientDetails();
+   
+    dashboardActions.submitCasePrint();
+    dashboardActions.returnToDashboard();
+    homeActions.isDashboardDisplayed();
+    dashboardActions.isUploadSuccesfully(0);
   });
   
-  it("55. Schedule Fee to Private", () => 
+  it("55. Private to Schedule Fee", () => 
+  {
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+    
+    //Add New Lesion - Patient Details
+    const firstname = `submit ${homeActions.randomAlpha(10)}`;
+    const lastname = `private to Schedule Fee`;
+    dashboardActions.selectClinicOptionByName();
+    dashboardActions.clickOkSelectClinic();
+    dashboardActions.clickAddNewLesion();
+    dashboardActions.selectTitle('Mrs');
+    dashboardActions.enterFirstName(firstname);
+    dashboardActions.enterLastName(lastname);
+    dashboardActions.selectGender('Unknown');
+    dashboardActions.enterDOB(user.DOB);
+    dashboardActions.enterHomeAdd(user.address);
+    dashboardActions.enterCity(user.city);
+    dashboardActions.selectState();
+    dashboardActions.enterPostcode(user.postcode);
+    dashboardActions.enterContact(user.contact);
+    dashboardActions.selectBilling(user.billing.private)
+    dashboardActions.checkPrivate();
+   
+    dashboardActions.nextButton();
+
+    //Add first lesion
+    dashboardActions.addALesionByNumberImages(1);
+
+    // Case Summary
+    dashboardActions.caseSummary();
+    dashboardActions.editToScheduleFee();
+   
+    dashboardActions.submitCasePrint();
+    dashboardActions.returnToDashboard();
+    homeActions.isDashboardDisplayed();
+    dashboardActions.isUploadSuccesfully(0);
+  });
+
+  it("56. Schedule Fee to Private", () => 
   {
     loginActions.visitPage();
     loginActions.inputUserName(user.username);
@@ -1410,52 +1456,6 @@ describe("Add New Lesion on eDerm", () => {
     dashboardActions.clickSavePatientDetails();
     dashboardActions.clickOkPatientDetails();
   
-    dashboardActions.submitCasePrint();
-    dashboardActions.returnToDashboard();
-    homeActions.isDashboardDisplayed();
-    dashboardActions.isUploadSuccesfully(0);
-  });
-    
-  it("56. Schedule Fee to DVA", () => 
-  {
-    loginActions.visitPage();
-    loginActions.inputUserName(user.username);
-    loginActions.inputPassword(user.password);
-    loginActions.clickLoginButton();
-    homeActions.isDashBoardButtonDisplayed();
-    
-    //Add New Lesion - Patient Details
-    const firstname = `submit ${homeActions.randomAlpha(10)}`;
-    const lastname = `Schedule Fee to DVA`;
-    dashboardActions.selectClinicOptionByName();
-    dashboardActions.clickOkSelectClinic();
-    dashboardActions.clickAddNewLesion();
-    dashboardActions.selectTitle('Mrs');
-    dashboardActions.enterFirstName(firstname);
-    dashboardActions.enterLastName(lastname);
-    dashboardActions.selectGender('Unknown');
-    dashboardActions.enterDOB(user.DOB);
-    dashboardActions.enterHomeAdd(user.address);
-    dashboardActions.enterCity(user.city);
-    dashboardActions.selectState();
-    dashboardActions.enterPostcode(user.postcode);
-    dashboardActions.enterContact(user.contact);
-    dashboardActions.fillScheduleFeeInPatientDetails();
-   
-    dashboardActions.nextButton();
-
-    //Add first lesion
-    dashboardActions.addALesionByNumberImages(1);
-
-    //Case Summary
-    dashboardActions.caseSummary();
-
-    dashboardActions.clickEditPatientDetails();
-    dashboardActions.selectBilling(user.billing.DVA)
-    dashboardActions.enterDVANumber(validDVANumber);
-    dashboardActions.clickSavePatientDetails();
-    dashboardActions.clickOkPatientDetails();
-   
     dashboardActions.submitCasePrint();
     dashboardActions.returnToDashboard();
     homeActions.isDashboardDisplayed();
