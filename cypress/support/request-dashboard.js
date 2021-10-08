a6f30cfa6377b021fbf38e4cf9543de2764e1096
loc nguyen <loc.nguyenqa28@gmail.com>
2021-10-08 15:44:41 +0700

    Updated saveDraftAPI

--- cypress/support/request-dashboard.js
+++ cypress/support/request-dashboard.js
@@ -17,7 +17,7 @@ Cypress.Commands.add('saveDraft', (
             cy.log(`Authen Key: ${authKey[0]}`)
             cy.request({
                 method: 'POST',
-                url: `https://sonic.ederm.com.au/api/createdraft?authkey=${authKey[0]}&firstname=${firstname}&title=Mr&lastname=Automation&gender=F&medicarenumber=2123-45670-1&dob=23/01/1969&address=test addd&city=01&patientfinancial=D&postcode=111&state=ACT&phone=123456`,
+                url: `/api/createdraft?authkey=${authKey[0]}&firstname=${firstname}&title=Mr&lastname=Automation&gender=F&medicarenumber=2123-45670-1&dob=23/01/1969&address=test addd&city=01&patientfinancial=D&postcode=111&state=ACT&phone=123456`,
                 body :{
                 }
             });
