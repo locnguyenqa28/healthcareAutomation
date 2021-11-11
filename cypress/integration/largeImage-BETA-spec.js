82d2bbad0187179febe367a45b19a2b35c65e55b
loc nguyen <loc.nguyenqa28@gmail.com>
2021-11-11 15:55:20 +0700

    added largeImage-BETA

---
+++ cypress/integration/largeImage-BETA-spec.js
@@ -0,0 +1,87 @@
+import { LoginActions } from "../pages/loginAction";
+import { HomeActions } from "../pages/homeAction";
+import { DashboardActions } from "../pages/dashboardActions";
+import user from "../support/constants"
+
+
+describe("Large image testing - BETA", () => {
+    const loginActions = new LoginActions();
+    const homeActions = new HomeActions();
+    const dashboardActions = new DashboardActions();
+
+  it("1. Multiple Large images - 10 lesions 4.7Mb x 3 images - 1", () => 
+  {
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+
+    dashboardActions.selectClinicOptionByName();
+    dashboardActions.clickOkSelectClinic();
+
+    //Add multiple
+    dashboardActions.addMuiltiPathologyRequestLargeImagesBySelectRegionV2('4.7Mb.jpg', 10, 5, 1);
+  });
+
+  it("2. Multiple Large images - 10 lesions 4.7Mb x 3 images - 2", () => 
+  {
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+
+    dashboardActions.selectClinicOptionByName();
+    dashboardActions.clickOkSelectClinic();
+
+    //Add multiple
+    dashboardActions.addMuiltiPathologyRequestLargeImagesBySelectRegionV2('4.7Mb.jpg', 10, 5, 2);
+  });
+
+  it("3. Multiple Large images - 10 lesions 4.7Mb x 3 images - 3", () => 
+  {
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+
+    dashboardActions.selectClinicOptionByName();
+    dashboardActions.clickOkSelectClinic();
+
+    //Add multiple
+    dashboardActions.addMuiltiPathologyRequestLargeImagesBySelectRegionV2('4.7Mb.jpg', 10, 5, 3);
+  });
+
+  it("4. Multiple Large images - 10 lesions 4.7Mb x 3 images - 4", () => 
+  {
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+
+    dashboardActions.selectClinicOptionByName();
+    dashboardActions.clickOkSelectClinic();
+
+    //Add multiple
+    dashboardActions.addMuiltiPathologyRequestLargeImagesBySelectRegionV2('4.7Mb.jpg', 10, 5, 4);
+  });
+
+  it("5. Multiple Large images - 10 lesions 4.7Mb x 3 images - 5", () => 
+  {
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+
+    dashboardActions.selectClinicOptionByName();
+    dashboardActions.clickOkSelectClinic();
+
+    //Add multiple
+    dashboardActions.addMuiltiPathologyRequestLargeImagesBySelectRegionV2('4.7Mb.jpg', 10, 5, 5);
+  });
+});
+  
