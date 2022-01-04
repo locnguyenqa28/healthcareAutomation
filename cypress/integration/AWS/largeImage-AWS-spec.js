39e4a866178dad3e22d5729f14ebf076830016a3
locnguyenqa28 <loc.nguyenqa28@gmail.com>
2022-01-04 22:21:34 +0700

    implement deletion cases

--- cypress/integration/AWS/largeImage-AWS-spec.js
+++ cypress/integration/AWS/largeImage-AWS-spec.js
@@ -68,5 +68,50 @@ describe("Large image testing - AWS", () => {
     //Add multiple
     dashboardActions.addMuiltiPathologyRequestLargeImagesBySelectRegionV2('4.9_2.jpg', 4, 1, 4);
   });
+
+  it.only("5. Submit the large images after Deleting a Lesion - 2 lesions 4.9Mb x 3 images", () => 
+  {
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+
+    dashboardActions.selectClinicOptionByName();
+    dashboardActions.clickOkSelectClinic();
+
+   //Add multiple
+   dashboardActions.addMuiltiPathologyRequestLargeImagesAndDeleteLesion('4.9_2.jpg', 2, 1, 2);
+  });
+
+  it.only("6. Submit the large images after Deleting a Lesion - 3 lesions 4.9Mb x 3 images", () => 
+  {
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+
+    dashboardActions.selectClinicOptionByName();
+    dashboardActions.clickOkSelectClinic();
+
+   //Add multiple
+   dashboardActions.addMuiltiPathologyRequestLargeImagesAndDeleteLesion('4.9_2.jpg', 3, 1, 3);
+  });
+
+  it.only("7. Submit the large images after Deleting a Lesion - 4 lesions 4.9Mb x 3 images", () => 
+  {
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+
+    dashboardActions.selectClinicOptionByName();
+    dashboardActions.clickOkSelectClinic();
+
+   //Add multiple
+   dashboardActions.addMuiltiPathologyRequestLargeImagesAndDeleteLesion('4.9_2.jpg', 4, 1, 4);
+  });
 });
   
