8d07ada0b64a1c0e9021cf913b8dcd97283d5863
locnguyenqa28 <loc.nguyenqa28@gmail.com>
2022-01-31 09:16:31 +0700

    implemented validation-clinic

--- cypress/integration/AWS/validation-spec.js
+++ cypress/integration/AWS/validation-spec.js
@@ -1,6 +1,7 @@
 import { LoginActions } from "../../pages/loginAction";
 import { HomeActions } from "../../pages/homeAction";
 import { DashboardActions } from "../../pages/dashboardActions";
+import { ClinicActions } from "../../pages/clinicActions";
 import user from "../../support/constants"
 
 
@@ -10,6 +11,8 @@ describe("Validation fields checking", () => {
     const loginActions = new LoginActions();
     const homeActions = new HomeActions();
     const dashboardActions = new DashboardActions();
+    const clinicActions = new ClinicActions();
+
     const validDVANumber = 'Abc12345678';
     const invalidSymbol = "%"
 
@@ -1042,6 +1045,94 @@ describe("Validation fields checking", () => {
     //Add another lesion
     dashboardActions.addAnotherLesion()
     dashboardActions.addAndValidateLesionCombineImages(5, 2)
+
+    //Case Summary
+    dashboardActions.caseSummary();
+    dashboardActions.submitCasePrint();
+    dashboardActions.returnToDashboard();
+    homeActions.isDashboardDisplayed();
+    dashboardActions.isUploadSuccesfully(0);
+  });
+ 
+  it.skip("Validation - Clinic - check all fields & submit 1 lesion", () => 
+  {
+    const clinicName = `validation${homeActions.randomAlpha(5)}`;
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+    
+    //Add New Clinic
+    dashboardActions.selectClinicOptionByName();
+    dashboardActions.clickOkSelectClinic();
+    dashboardActions.clickHrefByText('Setup');
+    clinicActions.clickHrefByText('Add new clinic');
+
+    // Clinic modal
+    clinicActions.isModal('Add new clinic');
+    clinicActions.clickSaveClinic();
+    // Assert message
+    clinicActions.assertPlaceholder('Please enter a clinic name.');
+    clinicActions.assertPlaceholder('Please enter a provider number. Allowed characters:A-Z,0-9.');
+    clinicActions.assertPlaceholder('Please enter an address.');
+    clinicActions.assertPlaceholder('Please enter a suburb.');
+    clinicActions.assertPlaceholder('Please enter a post code.');
+    clinicActions.assertPlaceholder('Invalid email format.');
+    clinicActions.assertPlaceholder('Please enter a phone.');
+    clinicActions.assertPlaceholder('Please enter a phone.');
+
+    clinicActions.enterClinicName(clinicName);
+    clinicActions.enterClinicProvidernumber();
+    clinicActions.enterClinicAddress();
+    clinicActions.enterClinicSubhub();
+    clinicActions.enterClinicPostcode();
+    clinicActions.selectClinicLab();
+    clinicActions.selectClinicState();
+    clinicActions.enterClinicPhone();
+    clinicActions.enterClinicMobilePhone();
+    clinicActions.enterClinicEmail();
+    clinicActions.clickSaveClinic();
+
+    // Assert clinic
+    clinicActions.reloadClinicPage();
+    clinicActions.isClinicTable();
+    clinicActions.isClinicName(clinicName);
+
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+    
+    //Add New Lesion - Patient Details
+    const firstname = `validation ${homeActions.randomAlpha(5)}`;
+    const lastname = `${homeActions.randomAlpha(10)}`;
+   
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+
+    dashboardActions.selectClinicOptionByName(clinicName);
+    dashboardActions.clickOkSelectClinic();
+    dashboardActions.clickAddNewLesion();
+
+    dashboardActions.selectTitle('Mrs');
+    dashboardActions.enterFirstName(firstname);
+    dashboardActions.enterLastName(lastname);
+    dashboardActions.selectGender('Unknown');
+    dashboardActions.enterDOB(user.DOB);
+    dashboardActions.enterHomeAdd(user.address);
+    dashboardActions.enterCity(user.city);
+    dashboardActions.selectState();
+    dashboardActions.enterPostcode(user.postcode);
+    dashboardActions.enterContact(user.contact);
+    dashboardActions.enterMedicare(user.medicare);
+    dashboardActions.nextButton();
+
+    dashboardActions.addALesionNoImage()
 
     //Case Summary
     dashboardActions.caseSummary();
