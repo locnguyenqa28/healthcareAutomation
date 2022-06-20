bd67857db0e62b1ea6760013b20b6512f0fb0ad7
locnguyenqa28 <loc.nguyenqa28@gmail.com>
2022-06-20 10:27:07 +0700

    refactored follow the cypress 10.

---
+++ cypress.config.js
@@ -0,0 +1,34 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  projectId: '6k3yms',
+  defaultCommandTimeout: 30000,
+  pageLoadTimeout: 120000,
+  chromeWebSecurity: false,
+  viewportWidth: 1366,
+  viewportHeight: 768,
+  failOnStatusCode: false,
+  env: {
+    base: 'https://sonic.ederm.com.au',
+    beta: 'http://localhost:3000',
+    allureResultsPath: 'report/results',
+  },
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    charts: true,
+    reportPageTitle: 'My Test Suite',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+  },
+  video: true,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'https://sonic.ederm.com.au',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
