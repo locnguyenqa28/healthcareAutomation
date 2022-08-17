1820d19a698c660f4a8a781136cbc4375856ecf4
locnguyenqa28 <loc.nguyenqa28@gmail.com>
2022-08-17 23:46:21 +0700

    added new cases for the timeout-extend

--- cypress/e2e/AWS/ClinicAndOthers/timeout-extend-spec.js
+++ cypress/e2e/AWS/ClinicAndOthers/timeout-extend-spec.js
@@ -421,4 +421,110 @@ describe("Timeout-extend", () => {
     dashboardActions.assertText(`Extend for ${timeoutLogin/2} minute`)
     dashboardActions.assertText('Log off and close')
   });
+ 
+  it("The timeout-extend popup able to visible at lesion 1 - completed", () => 
+  {
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+    
+    //Add New Lesion - Patient Details
+    const firstname = `extend-${homeActions.randomAlpha(10)}`;
+    const lastname = `${homeActions.randomAlpha(5)}`;
+    dashboardActions.selectClinicOptionByName();
+    dashboardActions.clickOkSelectClinic();
+    dashboardActions.clickAddNewLesion();
+    dashboardActions.selectTitle('Mrs');
+    dashboardActions.enterFirstName(firstname);
+    dashboardActions.enterLastName(lastname);
+    dashboardActions.selectGender('Male');
+    dashboardActions.enterDOB(user.DOB);
+    dashboardActions.enterHomeAdd(user.address);
+    dashboardActions.enterCity(user.city);
+    dashboardActions.selectState();
+    dashboardActions.enterPostcode(user.postcode);
+    dashboardActions.enterContact(user.contact);
+    dashboardActions.enterMedicare(user.medicare);
+    dashboardActions.nextButton();
+
+    
+    //Clinical Condition
+    dashboardActions.noPreviousHistory();
+    dashboardActions.provisionalDiagnosis();
+    dashboardActions.excludeMelasma();
+    dashboardActions.excludeNmsc();
+    dashboardActions.selectBiopsyType();
+    dashboardActions.addBodyMap();
+
+    //Case bodymap
+    dashboardActions.clickImage();
+    dashboardActions.selectBodyRegion();
+    dashboardActions.enterSpecimenLocation();
+    dashboardActions.saveBodyMap();
+
+    //Upload Dermascopic Images
+    dashboardActions.assertHeader('Upload ');
+    dashboardActions.uploadMultiImages(3);
+    cy.wait(500);
+    dashboardActions.assertText('Remove');
+    dashboardActions.startUpload();
+    dashboardActions.isProgressBarDisappear();
+    dashboardActions.isImageUploadedSuccessfully();
+    dashboardActions.nextButtonUploadImg(30000, true);
+
+    const timeoutLoginMs =  timeoutLogin * 60000
+    const extraTime = 30000
+    cy.wait(timeoutLoginMs/2)
+    cy.wait(extraTime)
+
+    dashboardActions.assertText(`You will be automatically logged out in ${timeoutLogin/2} minute`)
+    dashboardActions.assertText(`Extend for ${timeoutLogin/2} minute`)
+    dashboardActions.assertText('Log off and close')
+  });
+
+  it("The timeout-extend popup able to visible at lesion 2 completed", () => 
+  {
+    loginActions.visitPage();
+    loginActions.inputUserName(user.username);
+    loginActions.inputPassword(user.password);
+    loginActions.clickLoginButton();
+    homeActions.isDashBoardButtonDisplayed();
+    
+    //Add New Lesion - Patient Details
+    const firstname = `extend-${homeActions.randomAlpha(10)}`;
+    const lastname = `${homeActions.randomAlpha(5)}`;
+    dashboardActions.selectClinicOptionByName();
+    dashboardActions.clickOkSelectClinic();
+    dashboardActions.clickAddNewLesion();
+    dashboardActions.selectTitle('Mrs');
+    dashboardActions.enterFirstName(firstname);
+    dashboardActions.enterLastName(lastname);
+    dashboardActions.selectGender('Male');
+    dashboardActions.enterDOB(user.DOB);
+    dashboardActions.enterHomeAdd(user.address);
+    dashboardActions.enterCity(user.city);
+    dashboardActions.selectState();
+    dashboardActions.enterPostcode(user.postcode);
+    dashboardActions.enterContact(user.contact);
+    dashboardActions.enterMedicare(user.medicare);
+    dashboardActions.nextButton();
+
+   //Add first lesion
+   dashboardActions.addALesionByNumberImages(3)
+
+   //Add another lesion
+   dashboardActions.addAnotherLesion()
+   dashboardActions.addALesionByNumberImages(3)
+
+   const timeoutLoginMs =  timeoutLogin * 60000
+   const extraTime = 30000
+   cy.wait(timeoutLoginMs/2)
+   cy.wait(extraTime)
+
+   dashboardActions.assertText(`You will be automatically logged out in ${timeoutLogin/2} minute`)
+   dashboardActions.assertText(`Extend for ${timeoutLogin/2} minute`)
+   dashboardActions.assertText('Log off and close')
+  });
 });
